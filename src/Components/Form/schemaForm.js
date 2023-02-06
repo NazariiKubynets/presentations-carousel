3217a6b8ed078acaa2b0968f294f799387187a6e
NazarKubynets <evrik1330@gmail.com>
2023-02-06 14:54:32 +0200

    Add form

--- src/Components/Form/schemaForm.js
+++ src/Components/Form/schemaForm.js
@@ -3,18 +3,22 @@ import * as yup from "yup";
 export const schema = yup.object().shape({
    firstName: yup
       .string()
+      .max(25)
       .matches(/^([^0-9]*)$/, "First name should not contain numbers")
-      .matches(/^([A-z]*)$/, "Use the Latin alphabet")
+      .matches(/^([A-z]*)$/, "Use the Latin alphabet!")
       .required("First name is a required field"),
    lastName: yup
       .string()
+      .max(25)
       .matches(/^([^0-9]*)$/, "Last name should not contain numbers")
-      .matches(/^([A-z]*)$/, "Use the Latin alphabet")
+      .matches(/^([A-z]*)$/, "Use the Latin alphabet!")
       .required("Last name is a required field"),
    profession: yup
       .string()
+      .min(25)
+      .max(100)
       .required("Name profession is a required field")
-      .matches(/^([A-z- ]*)$/, "Use the Latin alphabet")
+      .matches(/^([A-z- ]*)$/, "Use the Latin alphabet!")
       .matches(/^([^0-9]*)$/, "Last profession should not contain numbers"),
    nameWebsite: yup
       .string()
@@ -23,8 +27,8 @@ export const schema = yup.object().shape({
       .required("Name company's website is a required field"),
    description: yup
       .string()
-      .required("Name company's website is a required field"),
-   // face: yup
-   //    .required("Name company's website is a required field")
-
+      .min(250)
+      .max(1000)
+      .matches(/[A-Za-z0-9_]/, "Use the Latin alphabet!")
+      .required("Description is a required field")
 });
--- src/store/postsReducer.js
+++ src/store/postsReducer.js
@@ -9,41 +9,41 @@ const defaultState = {
          "firstName": "Elon",
          "lastName": "Musk"
       },
-      "profession": "Генеральним директором та головним інженером компанії SpaceX та Tesla",
+      "profession": "CEO and Chief Engineer of SpaceX and Tesla",
       "site": "tesla.com, spacex.com",
       "img": {
          "face": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/274px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
          "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/1200px-Tesla_Motors.svg.png"
       },
-      "description": "Aмериканський підприємець, інженер та мільярдер. Він є засновником, генеральним директором та головним інженером компанії SpaceX; інвестором, генеральним директором та архітектором продукту компанії Tesla; засновником The Boring Company;співзасновником Neuralink та OpenAI; власником «Твіттер»."
+      "description": "American entrepreneur, engineer and billionaire. He is the founder, CEO and chief engineer of SpaceX; investor, CEO and product architect of Tesla; founder of The Boring Company; co-founder of Neuralink and OpenAI; owner of \"Twitter\"."
    },
    {
       "id": 2,
       "name": {
-         "firstName": "Eon",
-         "lastName": "Musk"
+         "firstName": "Mark",
+         "lastName": "Zuckerberg"
       },
-      "profession": "Генеральним директором та головним інженером компанії SpaceX та Tesla",
-      "site": "tesla.com, spacex.com",
+      "profession": "Co-founder and CEO of Meta and Breakthrough Starshotof",
+      "site": "meta.com, breakthroughinitiatives.org",
       "img": {
-         "face": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/274px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
-         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/1200px-Tesla_Motors.svg.png"
+         "face": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/800px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg",
+         "logo": "https://download.logo.wine/logo/Facebook/Facebook-Logo.wine.png"
       },
-      "description": "Aмериканський підприємець, інженер та мільярдер. Він є засновником, генеральним директором та головним інженером компанії SpaceX; інвестором, генеральним директором та архітектором продукту компанії Tesla; засновником The Boring Company;співзасновником Neuralink та OpenAI; власником «Твіттер»."
+      "description": "American media magnate, Internet entrepreneur and philanthropist. He is known as the co-founder of the company Meta (formerly Facebook, Inc.) and is its chairman, chief executive officer and controlling shareholder. He is also a co-founder of the Breakthrough Starshot solar sail spacecraft development project and one of the members of his board of directors."
    },
    {
       "id": 3,
       "name": {
-         "firstName": "on",
-         "lastName": "Musk"
+         "firstName": "Steve",
+         "lastName": "Jobs"
       },
-      "profession": "Генеральним директором та головним інженером компанії SpaceX та Tesla",
-      "site": "tesla.com, spacex.com",
+      "profession": "Entrepreneur, inventor, industrial designer, co-founder and CEO of Apple, NeXT and Pixar companies",
+      "site": "apple.com, pixar.com",
       "img": {
-         "face": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/274px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
-         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/1200px-Tesla_Motors.svg.png"
+         "face": "https://www.iphones.ru/wp-content/uploads/2021/06/20090909-wwdc-story-steve-jobs-02.jpg",
+         "logo": "https://www.svgrepo.com/show/162625/apple-logo.svg"
       },
-      "description": "Aмериканський підприємець, інженер та мільярдер. Він є засновником, генеральним директором та головним інженером компанії SpaceX; інвестором, генеральним директором та архітектором продукту компанії Tesla; засновником The Boring Company;співзасновником Neuralink та OpenAI; власником «Твіттер»."
+      "description": "American entrepreneur, inventor and industrial designer, widely recognized as a pioneer of the era of information technologies[5][6]. One of the founders, chairman of the board of directors and CEO of Apple Corporation. One of the founders and CEO of the Pixar film studio."
    }
    ],
    visibleModal: false
